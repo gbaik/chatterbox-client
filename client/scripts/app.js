466acbe37b5fa35e6b466016afce2687552fc171
Hack Reactor Student <student@hackreactor.com>
2017-05-26 16:17:35 -0700

    Add sending functionality to the app object within app.js. Adds crucial html elements to index.html

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,30 @@
-// YOUR CODE HERE:
+// const view = new View();
+// view.init();
+// console.log('app is loading');
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
+var app = {
+  init: function() {},
+  send: function(message) {
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+};
+
