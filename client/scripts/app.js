a53f68e187fae4abc105af98389e218aa6025206
Hack Reactor Student <student@hackreactor.com>
2017-05-26 18:47:10 -0700

    Fix send and fetch function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,29 +2,45 @@
 // view.init();
 // console.log('app is loading');
 
-// var message = {
-//   username: 'shawndrost',
-//   text: 'trololo',
-//   roomname: '4chan'
-// };
+var send = function(message) {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+var fetch = function() {
+  $.ajax({
+    url: app.server,
+    success: function (data) {
+      console.log('chatterbox: Message received', data);
+    },
+    error: function (data) {
+      console.log(app.server);
+      console.error('chatterbox: Failed to receive message', data);
+    }
+  });
+};
+
 
 var app = {
   init: function() {},
-  send: function(message) {
-    $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
-  }
+  send: send,
+  fetch: fetch,
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
+
 };
+
+
+
 
